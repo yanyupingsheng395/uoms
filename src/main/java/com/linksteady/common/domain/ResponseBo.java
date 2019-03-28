f88017b893e798e56791514b5545094175af6083
huang <huangkun@qfpay.com>
2019-03-28 14:31:29 +0800

    commit

--- src/main/java/com/linksteady/common/domain/ResponseBo.java
+++ src/main/java/com/linksteady/common/domain/ResponseBo.java
@@ -7,9 +7,9 @@ public class ResponseBo extends HashMap<String, Object> {
 	private static final long serialVersionUID = -8713837118340960775L;
 
 	// 成功
-	private static final Integer SUCCESS = 0;
+	private static final Integer SUCCESS = 200;
 	// 警告
-	private static final Integer WARN = 1;
+	private static final Integer WARN = 501;
 	// 异常 失败
 	private static final Integer FAIL = 500;
 
@@ -36,6 +36,15 @@ public class ResponseBo extends HashMap<String, Object> {
 		ResponseBo responseBo = new ResponseBo();
 		responseBo.put("code", SUCCESS);
 		responseBo.put("msg", msg);
+		return responseBo;
+	}
+
+	public static ResponseBo okOverPaging(Object msg,int totalCount,Object rows) {
+		ResponseBo responseBo = new ResponseBo();
+		responseBo.put("code", SUCCESS);
+		responseBo.put("msg", msg);
+		responseBo.put("total",totalCount);
+		responseBo.put("rows",rows);
 		return responseBo;
 	}
 
