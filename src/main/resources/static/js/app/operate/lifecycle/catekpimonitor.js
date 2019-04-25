da9a113a6eb30581579b70282bf9e7c467260a60
huixue <486860134@qq.com>
2019-04-25 19:28:19 +0800

    update

--- src/main/resources/static/js/app/operate/lifecycle/catekpimonitor.js
+++ src/main/resources/static/js/app/operate/lifecycle/catekpimonitor.js
@@ -1,172 +1,123 @@
-var xData = function() {
-    var data = [];
-    for (var i = 1; i < 13; i++) {
-        data.push(i);
+Date.prototype.format=function (){
+    var s='';
+    s+=this.getFullYear()+'/';          // 获取年份。
+    s+=(this.getMonth()+1)+"/";         // 获取月份。
+    s+= this.getDate();                 // 获取日。
+    return(s);                          // 返回日期。
+};
+//按日查询
+function getDayAll(begin,end){
+    var dateAllArr = new Array();
+    var ab = begin.split("-");
+    var ae = end.split("-");
+    var db = new Date();
+    db.setUTCFullYear(ab[0], ab[1]-1, ab[2]);
+    var de = new Date();
+    de.setUTCFullYear(ae[0], ae[1]-1, ae[2]);
+    var unixDb=db.getTime();
+    var unixDe=de.getTime();
+    for(var k=unixDb;k<=unixDe;){
+        dateAllArr.push((new Date(parseInt(k))).format().toString());
+        k=k+24*60*60*1000;
     }
-    return data;
+    return dateAllArr;
+}
+
+var xData = function() {
+    return getDayAll('2019-05-01', '2019-05-15');
 }();
 
 var option1 = {
-    "tooltip": {
-        "trigger": "axis",
-        "axisPointer": {
-            "type": "shadow",
+    tooltip: {
+        trigger: "axis",
+        axisPointer: {
+            type: "shadow",
             textStyle: {
                 color: "#fff"
             }
 
         },
     },
-    "grid": {
-        "borderWidth": 0,
-        "top": 110,
-        "bottom": 95,
+    grid: {
+        borderWidth: 0,
+        top: 110,
+        bottom: 95,
         textStyle: {
             color: "#fff"
         }
     },
-    "legend": {
+    legend: {
         right: 20,
         orient: 'vertical',
         textStyle: {
             color: '#90979c',
         },
-        "data": ['新客比例', '老客比例', 'GMV']
+        data: ['首购GMV', '非首购GMV']
 
     },
-    "calculable": true,
-    "xAxis": [{
-        "type": "category",
-        "axisLine": {
+    calculable: true,
+    xAxis: [{
+        name: "日期",
+        boundaryGap: false,
+        type: "category",
+        axisLine: {
             lineStyle: {
                 color: '#90979c'
             }
         },
-        "splitLine": {
+        splitLine: {
             "show": false
         },
-        "axisTick": {
-            "show": false
+        axisTick: {
+            show: false
         },
-        "splitArea": {
-            "show": false
+        splitArea: {
+            show: false
         },
-        "axisLabel": {
-            "interval": 0,
-
+        axisLabel: {
+            interval: 0,
+            // rotate: 40
         },
-        "data": xData,
+        data: xData
     }],
-    "yAxis": [{
-        "type": "value",
-        name: "新客比例/老客比例（%）",
-        "splitLine": {
-            "show": false
-        },
-        "axisLine": {
-            lineStyle: {
-                color: '#90979c'
-            }
-        },
-        "axisTick": {
-            "show": false
-        },
-        "axisLabel": {
-            "interval": 0,
-
-        },
-        "splitArea": {
-            "show": false
-        },
-
-    }, {
-        "type": "value",
+    yAxis: [{
+        type: "value",
         name: "GMV值（元）",
-        "splitLine": {
-            "show": false
+        splitLine: {
+            show: false
         },
-        "axisLine": {
+        axisLine: {
             lineStyle: {
                 color: '#90979c'
             }
         },
-        "axisTick": {
-            "show": false
+        axisTick: {
+            show: false
         },
-        "axisLabel": {
-            "interval": 0,
+        axisLabel: {
+            interval: 0,
 
         },
-        "splitArea": {
-            "show": false
+        splitArea: {
+            show: false
         },
 
     }],
-    "series": [{
-        "name": "新客比例",
-        "type": "bar",
-        "stack": "总量",
-        yAxisIndex:0,
-        "barMaxWidth": 35,
-        "barGap": "10%",
-        "itemStyle": {
-            "normal": {
-                "label": {
-                    "show": true,
-                    "textStyle": {
-                        "color": "#fff"
-                    },
-                    "position": "insideTop",
-                    formatter: function(p) {
-                        return p.value > 0 ? (p.value) : '';
-                    }
-                }
-            }
-        },
-        "data": getMonthRandom(0, 100),
-    },
-
-        {
-            "name": "老客比例",
-            "type": "bar",
-            "stack": "总量",
-            yAxisIndex:0,
-            "itemStyle": {
-                "normal": {
-                    "barBorderRadius": 0,
-                    "label": {
-                        "show": true,
-                        "position": "top",
-                        formatter: function(p) {
-                            return p.value > 0 ? (p.value) : '';
-                        }
-                    }
-                }
-            },
-            "data": getMonthRandom(0, 100)
-        }, {
-            "name": "GMV",
-            "type": "line",
-            "stack": "GMV",
-            yAxisIndex:1,
-            symbolSize:10,
-            symbol:'circle',
-            "itemStyle": {
-                "normal": {
-                    "barBorderRadius": 0,
-                    "label": {
-                        "show": true,
-                        "position": "top",
-                        formatter: function(p) {
-                            return p.value > 0 ? (p.value) : '';
-                        }
-                    }
-                }
-            },
-            "data": getMonthRandom(1000, 2000)
-        },
-    ]
+    series: [{
+        name: "首购GMV",
+        type: "line",
+        stack: "总量",
+        areaStyle: {normal: {}},
+        data: getMonthRandom(1000, 2000),
+    }, {
+        name: "非首购GMV",
+        type: "line",
+        areaStyle: {normal: {}},
+        stack: "总量",
+        data: getMonthRandom(1000, 2000)
+    }]
 };
+
 function getRandom (m,n){
     var num = Math.floor(Math.random()*(m - n) + n);
     return num;
@@ -174,7 +125,7 @@ function getRandom (m,n){
 
 function getMonthRandom(m,n) {
     var data = new Array();
-    for(var i=0; i<12; i++) {
+    for(var i=0; i<15; i++) {
         data.push(getRandom(m,n));
     }
     return data;
@@ -184,243 +135,209 @@ var chart1 = echarts.init(document.getElementById('chart1'), 'macarons');
 chart1.setOption(option1);
 
 var option2 = {
-    legend: {
-        right: 20,
-        orient: 'vertical',
-        data: ['老用户数','新用户数', '交易用户数']
-    },
     tooltip: {
-        trigger: 'axis',
+        trigger: "axis",
         axisPointer: {
-            lineStyle: {
-                color: '#ddd'
-            }
-        },
-        padding: [5, 10],
-        extraCssText: 'box-shadow: 0 0 5px rgba(0,0,0,0.3)'
-    },
-    xAxis: {
-        data: xData,
-        splitLine: {
-            show: false
-        }
-    },
-    yAxis: {
-        type: 'value',
-        splitLine: {
-            show: false
-        },
-        splitArea : {show : false}
-    },
-    series: [{
-        name: '老用户数',
-        type: 'line',
-        showSymbol: false,
-        symbol: 'circle',
-        symbolSize: 6,
-        data: ['1200', '1400', '1008', '1411', '1026', '1288', '1300', '800', '1100', '1000', '1118', '1322']
-    }, {
-        name: '新用户数',
-        type: 'line',
-        smooth: true,
-        showSymbol: false,
-        symbol: 'circle',
-        symbolSize: 6,
-        itemStyle: {
-            normal: {
-                color: '#00BFFF'
-            }
-        },
-        data: ['1200', '1400', '808', '811', '626', '488', '1600', '1100', '500', '300', '1998', '822']
-    }, {
-        name: '交易用户数',
-        type: 'line',
-        smooth: true,
-        showSymbol: false,
-        symbol: 'circle',
-        itemStyle: {
-            normal: {
-                color: '#f7b851'
-            }
-        },
-        symbolSize: 6,
-        data: ['', '', '', '1225', '1126', '1388', '1100', '1010', '1300', '1230', '1218', '1122']
-    }]
-};
-
-var chart2 = echarts.init(document.getElementById('chart2'), 'macarons');
-chart2.setOption(option2);
-
-var option3 = {
-    "tooltip": {
-        "trigger": "axis",
-        "axisPointer": {
-            "type": "shadow",
+            type: "shadow",
             textStyle: {
                 color: "#fff"
             }
 
         },
     },
-    "grid": {
-        "borderWidth": 0,
-        "top": 110,
-        "bottom": 95,
+    grid: {
+        borderWidth: 0,
+        top: 110,
+        bottom: 95,
         textStyle: {
             color: "#fff"
         }
     },
-    "legend": {
+    legend: {
         right: 20,
         orient: 'vertical',
         textStyle: {
             color: '#90979c',
         },
-        "data": ['平均件单价', '平均购买件数']
+        data: ['首购用户数', '非首购用户数']
 
     },
-    "calculable": true,
-    "xAxis": [{
-        name: "月份",
-        "type": "category",
-        "axisLine": {
+    calculable: true,
+    xAxis: [{
+        name: "日期",
+        boundaryGap: false,
+        type: "category",
+        axisLine: {
             lineStyle: {
                 color: '#90979c'
             }
         },
-        "splitLine": {
+        splitLine: {
             "show": false
         },
-        "axisTick": {
-            "show": false
+        axisTick: {
+            show: false
         },
-        "splitArea": {
-            "show": false
+        splitArea: {
+            show: false
         },
-        "axisLabel": {
-            "interval": 0,
+        axisLabel: {
+            interval: 0,
+            rotate: 40
 
         },
-        "data": xData,
+        grid: {
+            left: '10%',
+            bottom:'35%'
+        },
+        data: xData
     }],
-    "yAxis": [{
-        name: "平均件单价（元）",
-        "type": "value",
-        "splitLine": {
-            "show": false
+    yAxis: [{
+        type: "value",
+        name: "交易用户数",
+        splitLine: {
+            show: false
         },
-        "axisLine": {
+        axisLine: {
             lineStyle: {
                 color: '#90979c'
             }
         },
-        "axisTick": {
-            "show": false
+        axisTick: {
+            show: false
         },
-        "axisLabel": {
-            "interval": 0,
+        axisLabel: {
+            interval: 0,
 
         },
-        "splitArea": {
-            "show": false
-        },
-
-    }, {
-        name: "平均购买件数（件）",
-        "type": "value",
-        "splitLine": {
-            "show": false
-        },
-        "axisLine": {
-            lineStyle: {
-                color: '#90979c'
-            }
-        },
-        "axisTick": {
-            "show": false
-        },
-        "axisLabel": {
-            "interval": 0,
-
-        },
-        "splitArea": {
-            "show": false
+        splitArea: {
+            show: false
         },
 
     }],
-    "series": [{
-        "name": "平均购买件数",
-        yAxisIndex:1,
-        "type": "bar",
-        "stack": "平均购买件数",
-        "barMaxWidth": 35,
-        "barGap": "10%",
-        "itemStyle": {
-            "normal": {
-                "label": {
-                    "show": true,
-                    "textStyle": {
-                        "color": "#fff"
-                    },
-                    "position": "insideTop",
-                    formatter: function(p) {
-                        return p.value > 0 ? (p.value) : '';
-                    }
-                }
-            }
-        },
-        "data": [
-            578,
-            1069,
-            678,
-            788,
-            452,
-            607,
-            878,
-            656,
-            787,
-            487,
-            987,
-            787
-        ],
+    series: [{
+        name: "首购用户数",
+        type: "line",
+        stack: "总量",
+        areaStyle: {normal: {}},
+        data: getMonthRandom(1000, 2000),
     }, {
-        "name": "平均件单价",
-        yAxisIndex:0,
-        "type": "line",
-        "stack": "平均件单价",
-        symbolSize:10,
-        symbol:'circle',
-        "itemStyle": {
-            "normal": {
-                "barBorderRadius": 0,
-                "label": {
-                    "show": true,
-                    "position": "top",
-                    formatter: function(p) {
-                        return p.value > 0 ? (p.value) : '';
-                    }
-                }
+        name: "非首购用户数",
+        type: "line",
+        areaStyle: {normal: {}},
+        stack: "总量",
+        data: getMonthRandom(1000, 2000)
+    }]
+};
+
+var chart2 = echarts.init(document.getElementById('chart2'), 'macarons');
+chart2.setOption(option2);
+
+
+var data_1 = getMonthRandom(1000, 2000);
+var data_2 = getMonthRandom(1000, 2000);
+var data_3 = arrPlus(data_1, data_2);
+
+function arrPlus(arr1, arr2) {
+    var arr = new Array();
+    $.each(arr1, function (k, v) {
+        arr.push(v + arr2[k]);
+    });
+    return arr;
+}
+var option3 = {
+    tooltip: {
+        trigger: "axis",
+        axisPointer: {
+            type: "shadow",
+            textStyle: {
+                color: "#fff"
+            }
+
+        },
+    },
+    grid: {
+        borderWidth: 0,
+        top: 110,
+        bottom: 95,
+        textStyle: {
+            color: "#fff"
+        }
+    },
+    legend: {
+        right: 20,
+        orient: 'vertical',
+        textStyle: {
+            color: '#90979c',
+        },
+        data: ['首购平均客单价', '非首购平均客单价', '平均客单价']
+
+    },
+    calculable: true,
+    xAxis: [{
+        name: "日期",
+        boundaryGap: false,
+        type: "category",
+        axisLine: {
+            lineStyle: {
+                color: '#90979c'
             }
         },
-        "itemStyle": {
-            "normal": {
-                "color": '#f7b851'
+        splitLine: {
+            "show": false
+        },
+        axisTick: {
+            show: false
+        },
+        splitArea: {
+            show: false
+        },
+        axisLabel: {
+            interval: 0,
+            rotate: 40
+
+        },
+        grid: {
+            left: '10%',
+            bottom:'35%'
+        },
+        data: xData
+    }],
+    yAxis: [{
+        type: "value",
+        name: "平均客单价（元）",
+        splitLine: {
+            show: false
+        },
+        axisLine: {
+            lineStyle: {
+                color: '#90979c'
             }
         },
-        "data": [
-            540,
-            1024,
-            1356,
-            1267,
-            1368,
-            1027,
-            1125,
-            1136,
-            1264,
-            1056,
-            1149,
-            1087
-        ]
+        axisTick: {
+            show: false
+        },
+        axisLabel: {
+            interval: 0,
+
+        },
+        splitArea: {
+            show: false
+        },
+
+    }],
+    series: [{
+        name: "首购平均客单价",
+        type: "line",
+        data: data_1,
+    }, {
+        name: "非首购平均客单价",
+        type: "line",
+        data: data_2
+    }, {
+        name: "平均客单价",
+        type: "line",
+        data: data_3
     }]
 };
 
