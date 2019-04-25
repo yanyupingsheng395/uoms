55fabf1c10ca007e9e4963f64927bd8229472a4b
huixue <486860134@qq.com>
2019-04-25 11:49:58 +0800

    update

--- src/main/resources/static/js/app/operate/kpimonitor/cohort.js
+++ src/main/resources/static/js/app/operate/kpimonitor/cohort.js
@@ -4,7 +4,11 @@ $(function () {
 
     init_date("startDate_1", "yyyy-mm-dd", 0,2,0);
     init_date("endDate_1", "yyyy-mm-dd", 0,2,0);
+
+    init_date("startDate_2", "yyyy-mm-dd", 0,2,0);
+    init_date("endDate_2", "yyyy-mm-dd", 0,2,0);
     getData(1);
+    getData1(1);
 });
 
 // 时间维度的click
@@ -19,10 +23,29 @@ $("#cohortbtngroup1>.cohortbtn").on('click',function () {
     getData(indexTotal);
 });
 
+
+$("#cohortbtngroup2>.cohortbtn").on('click',function () {
+    //判断当前元素是否具有btn-primary 的class属性 如果已经有，则什么也不做
+    if(!$(this).hasClass("btn-primary"))
+    {
+        //修改当前元素的样式
+        $(this).removeClass("btn-default").addClass("btn-primary");
+        $(this).siblings().removeClass("btn-primary").addClass("btn-default");
+    }
+    getData1(indexTotal1);
+});
+
 var indexTotal = 1;
+var indexTotal1 = 1;
+
 function tabClick(idx) {
     indexTotal = idx;
     getData(idx);
+}
+
+function tab1Click(idx) {
+    indexTotal1 = idx;
+    getData1(idx);
 }
 
 // 获取表格数据
@@ -42,8 +65,6 @@ function getData(idx) {
     var end = $("#endDate_1").val();
     var periodType=$("#cohortbtngroup1>.btn-primary:first").attr("name");
     $.get(url, {periodType: periodType, start: start, end: end}, function(r) {
-        console.log(r);
-
         var columns = r.data.columns;
         var data = r.data.data;
 
@@ -94,6 +115,69 @@ function getData(idx) {
     });
 }
 
+
+function getData1(idx) {
+    var url = "";
+    if(idx == 1) {
+        url = "/kpiMonitor/getUpriceData";
+    }else if(idx == 2) {
+        url = "/kpiMonitor/getSpriceData"
+    }else if(idx == 3) {
+        url = "/kpiMonitor/getJoinrateData"
+    }else if(idx == 4) {
+        url = "/kpiMonitor/getFreqData"
+    }
+    var $table = $('#dataTable1' + idx);
+    var start = $("#startDate_2").val();
+    var end = $("#endDate_2").val();
+    var periodType=$("#cohortbtngroup2>.btn-primary:first").attr("name");
+    $.get(url, {periodType: periodType, start: start, end: end}, function(r) {
+        var columns = r.data.columns;
+        var data = r.data.data;
+
+        var tmp = new Array();
+        $.each(columns, function (k, v) {
+            if(v == "month") {
+                tmp.push({
+                    field: v,
+                    title: '月份',
+                    width: '132px',
+                    sortable: false
+                });
+            }else if(v == "week") {
+                tmp.push({
+                    field: v,
+                    title: '周',
+                    width: '132px',
+                    sortable: false
+                });
+            }else if(v == "newuser") {
+                tmp.push({
+                    field: v,
+                    title: '本月新增用户数',
+                    width: '132px',
+                    sortable: false
+                });
+            }else {
+                tmp.push({
+                    field: v,
+                    title: v,
+                    width: '132px',
+                    sortable: false,
+                    formatter: function (value, row, index) {
+                        if(value == "-1") {
+                            return "";
+                        }else {
+                            return value;
+                        }
+                    }
+                });
+            }
+        });
+        initBootstrapTable($table, tmp, data, r.data.total, periodType);
+    });
+}
+
 // 初始化dataTable
 function initBootstrapTable($el, columns, data, total, periodType) {
     var option = {
@@ -101,7 +185,8 @@ function initBootstrapTable($el, columns, data, total, periodType) {
         data: data,
         search: false,
         fixedColumns: true,
-        fixedNumber: 2
+        fixedNumber: 2,
+        height:400
     };
     // if(data.length > 12) {
     //     option.height = 400;
