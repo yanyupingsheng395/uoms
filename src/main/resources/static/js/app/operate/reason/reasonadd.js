a0c1e9b899fc696f4c944359b1ab0c8adeb69fa2
huixue <486860134@qq.com>
2019-04-28 17:37:13 +0800

    update

--- src/main/resources/static/js/app/operate/reason/reasonadd.js
+++ src/main/resources/static/js/app/operate/reason/reasonadd.js
@@ -231,7 +231,7 @@ function saveDim() {
 
     console.log(inputValue);
     var template="<li class='list-group-item'><input  class='col-xs-11 dimDispaly' value=\""+inputValue+"\"  title=\""+inputValue+"\" style='border:0px' disabled='true'/>" +
-        "<input type='hidden' class='dimKey' value='"+selectDim+"'/><input type='hidden' class='dimValues' value='"+hiddenValue+"' /><span class='mdi mdi-delete dim-remove' style='color: #006cfa;'></span></li>";
+        "<input type='hidden' class='dimKey' value='"+selectDim+"'/><input type='hidden' class='dimValues' value='"+hiddenValue+"' /><span class='mdi mdi-delete dim-remove' style='color: #f96868;'></span></li>";
     $("#dimlist").append(template);
 
     $("#dim_modal").modal('hide');
