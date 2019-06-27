284287a2c651a444e9e95eab74497305d1a571c0
huixue <486860134@qq.com>
2019-06-27 11:38:37 +0800

    update

--- mdss/src/main/java/com/linksteady/operate/config/SystemProperties.java
+++ mdss/src/main/java/com/linksteady/operate/config/SystemProperties.java
@@ -5,7 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties(prefix = "mdss")
+@ConfigurationProperties(prefix = "sys")
 public class SystemProperties {
 
     private ShiroProperties shiro = new ShiroProperties();
