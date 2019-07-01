af61bb15789e24bcddfc8d5bb0fb23250f98b77a
huixue <486860134@qq.com>
2019-07-01 17:15:26 +0800

    删除无效代码

--- mdss/src/main/java/com/linksteady/operate/service/DiagConditionService.java
+++ mdss/src/main/java/com/linksteady/operate/service/DiagConditionService.java
@@ -2,7 +2,4 @@ package com.linksteady.operate.service;
 
 public interface DiagConditionService {
 
-    void redisCreate(String data, String diagId, String nodeId) throws Exception;
-
-    String redisRead(String diagId, String nodeId) throws Exception;
 }
--- mdss/src/main/java/com/linksteady/operate/service/impl/DiagConditionServiceImpl.java
+++ mdss/src/main/java/com/linksteady/operate/service/impl/DiagConditionServiceImpl.java
@@ -12,23 +12,4 @@ import org.springframework.stereotype.Service;
 @Service
 public class DiagConditionServiceImpl implements DiagConditionService {
 
-    @Autowired
-    private RedisSessionDAO redisSessionDAO;
-
-    private static final String PREFIX = "node_";
-
-    @Override
-    public void redisCreate(String data, String diagId, String nodeId) throws Exception{
-        JSONArray jsonArray = JSON.parseArray(data);
-        RedisSerializer serializer = new StringSerializer();
-        String key = PREFIX + diagId + "_" + nodeId;
-        redisSessionDAO.getRedisManager().set(serializer.serialize(key), serializer.serialize(jsonArray), 1800);
-    }
-
-    @Override
-    public String redisRead(String diagId, String nodeId) throws Exception{
-        RedisSerializer serializer = new StringSerializer();
-        byte[] result = redisSessionDAO.getRedisManager().get(serializer.serialize(PREFIX + diagId + "_" + nodeId));
-        return ((StringSerializer) serializer).deserialize(result);
-    }
 }
