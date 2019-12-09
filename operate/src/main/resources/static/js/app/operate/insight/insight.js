16087c318bfd9efd8d1af4c064ed1a73f9f7ee60
huixue <486860134@qq.com>
2019-12-09 18:18:10 +0800

    5个指标echart图

--- operate/src/main/resources/static/js/app/operate/insight/insight.js
+++ operate/src/main/resources/static/js/app/operate/insight/insight.js
@@ -2,6 +2,7 @@ $( function () {
     findUserCntList();
     findSpuValueList();
     findImportSpu();
+    kpiInit();
 } );
 
 // 按时间范围查询
@@ -9,6 +10,7 @@ function searchInsight() {
     var dateRange = $("#dateRange").val();
     findUserCntList();
     $("#sankeyFrame").attr("src", "/sankey?dateRange=" + dateRange);
+    $("#breakBtn").attr("href", "/sankey?dateRange=" + dateRange);
 }
 
 function resetInsight() {
@@ -204,4 +206,250 @@ function searchImportSpu() {
 function resetImportSpu() {
     $("input[name='spuName']").val("");
     $("select[name='purchOrder']").find("option:selected").removeAttr("selected");
+}
+
+// 5个指标随购买次数变化曲线
+
+var setting = {
+    async: {
+        enable: true,
+        url: getUrl
+    },
+    check: {
+        enable: true,
+        chkStyle: "radio",
+        radioType: "all"
+    },
+    view: {
+        dblClickExpand: false,
+        showIcon: false
+    },
+    data: {
+        simpleData: {
+            enable: true
+        }
+    },
+    callback: {
+        onClick: onClick,
+        onCheck: onCheck,
+        beforeExpand: beforeExpand,
+        onAsyncSuccess: onAsyncSuccess,
+        onAsyncError: onAsyncError
+    }
+};
+
+function onClick(e, treeId, treeNode) {
+    var zTree = $.fn.zTree.getZTreeObj("spuTree");
+    zTree.checkNode(treeNode, !treeNode.checked, null, true);
+    return false;
+}
+
+function onCheck(e, treeId, treeNode) {
+    var zTree = $.fn.zTree.getZTreeObj("spuTree"),
+        nodes = zTree.getCheckedNodes(true),
+        v = "";
+    for (var i=0, l=nodes.length; i<l; i++) {
+        v += nodes[i].name + ",";
+    }
+    if (v.length > 0 ) v = v.substring(0, v.length-1);
+    var spuOrProductName = $("#spuOrProductName");
+    spuOrProductName.attr("value", v);
+    $("#id").val(nodes[0].id);
+    $("#menuContent").toggle();
+    if(treeNode.isParent) {
+        $("#type").val("spu");
+    }else {
+        $("#type").val("product");
+    }
+}
+
+function beforeExpand(treeId, treeNode) {
+    if (!treeNode.isAjaxing) {
+        ajaxGetNodes(treeNode, "refresh");
+        return true;
+    } else {
+        $MB.n_warning("正在下载数据中，请稍后展开节点...");
+        return false;
+    }
+}
+var className = "dark";
+function onAsyncSuccess(event, treeId, treeNode, msg) {
+    if (!msg || msg.length == 0) {
+        return;
+    }
+    var zTree = $.fn.zTree.getZTreeObj("spuTree"),
+        totalCount = treeNode.count;
+    if (treeNode.children.length < totalCount) {
+        setTimeout(function() {ajaxGetNodes(treeNode);}, perTime);
+    } else {
+        treeNode.icon = "";
+        zTree.updateNode(treeNode);
+        zTree.selectNode(treeNode.children[0]);
+        className = (className === "dark" ? "":"dark");
+    }
+}
+function onAsyncError(event, treeId, treeNode, XMLHttpRequest, textStatus, errorThrown) {
+    var zTree = $.fn.zTree.getZTreeObj("spuTree");
+    $MB.n_danger("异步获取数据出现异常。");
+    treeNode.icon = "";
+    zTree.updateNode(treeNode);
+}
+function ajaxGetNodes(treeNode, reloadType) {
+    var zTree = $.fn.zTree.getZTreeObj("spuTree");
+    if (reloadType == "refresh") {
+        zTree.updateNode(treeNode);
+    }
+    zTree.reAsyncChildNodes(treeNode, reloadType, true);
+}
+
+// 指标随购买次数变化曲线初始化条件
+function kpiInit() {
+    var left = $("#spuOrProductName").offset().left - $(".row").offset().left - 14;
+    var width = $("#spuOrProductName").width() + 5;
+    $("#spuTree").attr("style", "margin-left:"+ left +"px;width:"+width+"px;");
+    initZtree();
+}
+
+const promise = new Promise(function (resolve, reject) {
+    $.get("/insight/getSpuTree", {}, function (r){
+        if(r.code === 200) {
+            resolve(r);
+        } else {
+            reject(r);
+        }
+    });
+});
+
+async function initZtree() {
+    let result = await promise;
+    $.fn.zTree.init($("#spuTree"), setting, result.data);
+}
+
+$("#spuOrProductName").click(
+    function () {
+        $("#menuContent").toggle();
+    }
+);
+
+function getUrl(treeId, treeNode) {
+    return "/insight/getProductTree?spuWid=" + treeNode.id;
+}
+
+/**
+ * 留存率随购买次数的变化图
+ */
+function retentionInPurchaseTimes() {
+    $.get("/insight/retentionInPurchaseTimes", {id: $("#id").val(), type: $("#type").val(), period: $("#period").val()}, function (r) {
+        var option = getOption(r.data, "留存率");
+        var chart = echarts.init(document.getElementById("chart1"), 'macarons');
+        chart.setOption(option);
+    });
+}
+
+/**
+ * 件单价随购买次数变化
+ */
+function unitPriceInPurchaseTimes() {
+    $.get("/insight/unitPriceInPurchaseTimes", {id: $("#id").val(), type: $("#type").val(), period: $("#period").val()}, function (r) {
+        var option = getOption(r.data, "件单价");
+        var chart = echarts.init(document.getElementById("chart3"), 'macarons');
+        chart.setOption(option);
+    });
+}
+
+/**
+ * 连带率随购买次数变化
+ */
+function joinRateInPurchaseTimes() {
+    $.get("/insight/joinRateInPurchaseTimes", {id: $("#id").val(), type: $("#type").val(), period: $("#period").val()}, function (r) {
+        var option = getOption(r.data, "连带率");
+        var chart = echarts.init(document.getElementById("chart4"), 'macarons');
+        chart.setOption(option);
+    });
+}
+
+/**
+ * 品类种数随购买次数变化
+ */
+function categoryInPurchaseTimes() {
+    $.get("/insight/categoryInPurchaseTimes", {id: $("#id").val(), type: $("#type").val(), period: $("#period").val()}, function (r) {
+        var option = getOption(r.data, "品类种数");
+        var chart = echarts.init(document.getElementById("chart5"), 'macarons');
+        chart.setOption(option);
+    });
+}
+
+/**
+ * 时间间隔随购买次数变化
+ */
+function periodInPurchaseTimes() {
+    $.get("/insight/periodInPurchaseTimes", {id: $("#id").val(), type: $("#type").val(), period: $("#period").val()}, function (r) {
+        var option = getOption(r.data, "时间间隔");
+        var chart = echarts.init(document.getElementById("chart6"), 'macarons');
+        chart.setOption(option);
+    });
+}
+
+function getOption(data, name) {
+    return option = {
+        xAxis: {
+            name: '购买次数',
+            type: 'category',
+            data: data.xdata,
+            splitLine:{show: false},
+            splitArea : {show : false}
+        },
+        yAxis: {
+            name: name,
+            type: 'value',
+            splitLine:{show: false},
+            splitArea : {show : false}
+        },
+        series: [{
+            data: data.ydata,
+            type: 'line'
+        }],
+        grid: {right:'15%'},
+        title: {
+            text: name + '随购买次数变化',
+            x: 'center',
+            y: 'bottom',
+            textStyle: {
+                //文字颜色
+                color: '#000',
+                //字体风格,'normal','italic','oblique'
+                fontStyle: 'normal',
+                //字体粗细 'normal','bold','bolder','lighter',100 | 200 | 300 | 400...
+                fontWeight: 'normal',
+                //字体系列
+                fontFamily: 'sans-serif',
+                //字体大小
+                fontSize: 12
+            }
+        }
+    }
+}
+
+function searchRetention() {
+    if($("#id").val() === '' || $("#period").val() === '') {
+        $MB.n_warning("请选择商品/SPU，查询时间周期。");
+        return false;
+    }
+    $("#mask").hide();
+    $("#charts").show();
+    retentionInPurchaseTimes();
+    unitPriceInPurchaseTimes();
+    joinRateInPurchaseTimes();
+    categoryInPurchaseTimes();
+    periodInPurchaseTimes();
+}
+
+// 重置留存率的条件
+function resetRetention() {
+    $("#mask").show();
+    $("#charts").hide();
+    $("#spuOrProductName").val("");
+    $("#id").val("");
+    $("#type").val("");
+    $("#period").find("option:selected").removeAttr("selected");
 }
