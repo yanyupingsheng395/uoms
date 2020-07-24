637ea8fc79472ad5a252d7bcabd68b651af8f2cc
huixue <486860134@qq.com>
2020-07-24 10:56:28 +0800

    update bug

--- operate/src/main/java/com/linksteady/operate/service/impl/ActivityUserGroupServiceImpl.java
+++ operate/src/main/java/com/linksteady/operate/service/impl/ActivityUserGroupServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 import java.util.stream.Collectors;
 
