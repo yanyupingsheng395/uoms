5cdc6d0b35e888edbd575e984e6816154de8710f
huang <huangkun@qfpay.com>
2020-11-17 14:38:35 +0800

    1. 更新参数的修改方法

--- qywxApi/src/main/java/com/linksteady/qywx/controller/ExternalContactEventController.java
+++ qywxApi/src/main/java/com/linksteady/qywx/controller/ExternalContactEventController.java
@@ -1,16 +1,12 @@
 package com.linksteady.qywx.controller;
 
-import com.linksteady.common.domain.ResponseBo;
 import com.linksteady.common.util.crypto.SHA1;
-import com.linksteady.qywx.service.ExternalContactService;
 import com.linksteady.qywx.service.QywxService;
 import com.linksteady.qywx.utils.EventCryptUtils;
+import com.linksteady.qywx.vo.WxXmlMessage;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 外部联系人事件
@@ -47,5 +43,32 @@ public class ExternalContactEventController {
         String content=EventCryptUtils.decrypt(qywxService.getEcEventAesKey(),echostr);
         log.info("\n消息解密后内容为：\n{} ", content);
         return content;
+    }
+
+    /**
+     * 接收事件 (处理程序)
+     * @return
+     */
+    @PostMapping("/event")
+    public String eventForPost(@RequestBody(required = false) String requestBody,
+                               @RequestParam("timestamp") String timestamp,
+                               @RequestParam("nonce") String nonce,
+                               @RequestParam("msg_signature") String msgSignature) {
+        log.info(
+                "\n接收企业微信系统事件：[msgSignature=[{}],"
+                        + " timestamp=[{}], nonce=[{}], requestBody=[\n{}\n] ", msgSignature, timestamp, nonce, requestBody);
+
+        // 对消息进行解密
+        WxXmlMessage inMessage =EventCryptUtils.decrypt(
+                qywxService.getEcEventToken(),
+                qywxService.getEcEventAesKey(),
+                msgSignature,
+                timestamp,
+                nonce,
+                requestBody
+        );
+
+        log.debug("\n消息解密后内容为：\n{} ", inMessage.toString());
+        return null;
     }
 }
